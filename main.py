75fdf47f931951aa469b87f34b8642147cb6705b
melih <durmazmelih.ml@gmail.com>
2024-08-08 17:16:52 +0300

    ewrrwe

--- main.py
+++ main.py
@@ -14,25 +14,25 @@ app.add_middleware(
     allow_methods=["*"],  # Tüm HTTP yöntemlerine izin vermek için
     allow_headers=["*"],  # Tüm başlıklara izin vermek için
 )
-port=os.environ.get("PORT",5000)
+
 
 @app.get("/")
 async def test():
-    await start_telegram_bot()
+    # await start_telegram_bot()
     return "ok"
 
-@app.get("/pointsAndClickPower/{user_id}")
-async def get_points(user_id: int):
-    result = get_user_points(user_id)
-    if result:
-        return result
-    raise HTTPException(status_code=404, detail="User not found")
+# @app.get("/pointsAndClickPower/{user_id}")
+# async def get_points(user_id: int):
+#     result = get_user_points(user_id)
+#     if result:
+#         return result
+#     raise HTTPException(status_code=404, detail="User not found")
 
-@app.get("/update-points")
-def update_points(telegramid: int, points: int, clickpower: int, bar: int):
-    update_user_points(telegramid, points, clickpower, bar)
-    return {"message": "Points updated successfully"}
+# @app.get("/update-points")
+# def update_points(telegramid: int, points: int, clickpower: int, bar: int):
+#     update_user_points(telegramid, points, clickpower, bar)
+#     return {"message": "Points updated successfully"}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0",port=port, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
