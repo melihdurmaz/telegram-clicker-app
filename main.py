51a873964ed356f8d8c8459bcea065fe46da74d7
melih <durmazmelih.ml@gmail.com>
2024-08-08 14:54:05 +0300

    dneeme3

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import nest_asyncio
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from telegram.ext import Application, CommandHandler
-from service import start
-from service import get_user_points, update_user_points
+from service import start, get_user_points, update_user_points
 from fastapi.middleware.cors import CORSMiddleware
 from config import API_TOKEN
 
@@ -17,9 +16,10 @@ app.add_middleware(
     allow_methods=["*"],  # Tüm HTTP yöntemlerine izin vermek için
     allow_headers=["*"],  # Tüm başlıklara izin vermek için
 )
+
 async def start_telegram_bot() -> None:
     application = Application.builder().token(API_TOKEN).build()
-    start_handler = CommandHandler('start',start)
+    start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
     await application.initialize()
     await application.start()
@@ -37,13 +37,12 @@ async def get_points(user_id: int):
     raise HTTPException(status_code=404, detail="User not found")
 
 @app.get("/update-points")
-def update_points(telegramid:int,points:int,clickpower:int,bar:int):
-    update_user_points(telegramid,points,clickpower,bar)
+def update_points(telegramid: int, points: int, clickpower: int, bar: int):
+    update_user_points(telegramid, points, clickpower, bar)
     return {"message": "Points updated successfully"}
-   
 
 async def start_fastapi():
-    config = uvicorn.Config("main:app", port=8000, reload=True)
+    config = uvicorn.Config(app, port=8000, reload=True)
     server = uvicorn.Server(config)
     await server.serve()
 
@@ -51,10 +50,8 @@ async def main():
     await asyncio.gather(
         start_fastapi(),
         start_telegram_bot(),
-    )  
+    )
 
 if __name__ == "__main__":
     nest_asyncio.apply()
     asyncio.run(main())
-    
-    
